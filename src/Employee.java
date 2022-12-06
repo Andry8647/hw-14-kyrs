5e5e08cca98a2f4e8677e8e0c154d3ad1797fd32
Андрей <honor10love2@gmail.com>
2022-12-06 16:30:54 +0300

    Второй коммит, поправил замечания по курсовой

--- src/Employee.java
+++ src/Employee.java
@@ -1,40 +1,48 @@
 public class Employee {
-     String surname;
-     String name;
-     String patronymic;
-     int department;
-     int pay;
-    static int id = 0;
+    private final String surname;
+    private final String name;
+    private final String patronymic;
+    private int department;
+    private int pay;
+    private static int id;
+    private static int counter = 0;
     public Employee(String surname,String name,String patronymic, int department, int pay){
         this.surname = surname;
         this.name=name;
         this.patronymic = patronymic;
         this.department = department;
         this.pay = pay;
+        this.id = counter++;
 
     }
     public static void printEmployee(Employee[] employees){ //Печать всех сотрудников по списку
         for (Employee employee: employees){ //использовал foreach
             System.out.println(employee);
+
         }
     }
     public static void sumPayEmployee(Employee[] employees){ // метод для нахождения суммы всех зарплат
         double sum = 0;
-           for (int i = 0; i < employees.length;i++){ //Здесь решил через обычную конструкцию
-              Employee employee = employees[i];
-              sum = employee.getPay() + sum; //складываю все значения
+           for (int i = 0; i < employees.length;i++) { //Здесь решил через обычную конструкцию
+               if (employees[i] != null) {
+                   Employee employee = employees[i];
+                   sum = employee.getPay() + sum; //складываю все значения
+               }
            }
            double average = sum/ employees.length;// Нахожу среднюю величину зарплат
         System.out.println("Среднее значения зарплаты: " + average);
         System.out.println("Сумма всех зарплат сотрудников компании: " + sum);// вывод суммы всех зарплат сотрудников
+
     }
     public static void minPayEmployee(Employee[] employees){//метод для нахождения минимальной зарплаты
         int min = 100000000;//такое большое число для того чтобы первая зарплата сразу же была меньше и зачислилась в переменную min
-        for (int i = 0; i< employees.length;i++){
-            Employee employee = employees[i];
-            if(employee.getPay() < min){ //условия для нахождения минимального значения
-                min = employee.getPay();
-                id = i;// номер сотрудника с наименьшей зарплатой
+        for (int i = 0; i< employees.length;i++) {
+            if (employees[i] != null) {
+                Employee employee = employees[i];
+                if (employee.getPay() < min) { //условия для нахождения минимального значения
+                    min = employee.getPay();
+                    id = i;// номер сотрудника с наименьшей зарплатой
+                }
             }
         }
         System.out.println(employees[id] + " Это минимальная зарплата сотрудников компании.");//Печать минимального значения
@@ -42,23 +50,26 @@ public class Employee {
     public static void maxPayEmployee(Employee[] employees){//Нахождение максимального значения
         int max = -100000000;//такое маленькое число для того чтобы первая же сразу зарплата попала в переменную
         for (int i = 0; i < employees.length;i++){
+            if (employees[i] != null) {
             Employee employee = employees[i];
             if(employee.getPay() > max){// проверка на зарплату
                 max = employee.getPay();
                 id = i; // номер сотрудника с наибольшей зарплатой
             }
         }
+        }
         System.out.println(employees[id] + " Это максимальная зарплата сотрудников компании.");
     }
     public static void nameSurnamePatronymic(Employee[] employees){// метод прописывающий ФИО
         for (int i = 0; i < employees.length;i++) {
+            if (employees[i] != null) {
             Employee employee = employees[i];
             System.out.println("Фамилия: " + employee.getSurname() + "Имя: " + employee.getName() + "Отчество: " + employee.getPatronymic());
         }
+            }
     }
-    public int getId(){
-        return this.id;
-    }
+
+
 
     public String getName(){
         return this.name;
@@ -85,4 +96,5 @@ public class Employee {
     public String toString() {
         return "Имя сотрудника: "+ surname + name + patronymic +" Отдел: "+ department + " Зарплата: " + pay;
     }
+
 }
